898f307302983e5f54283c7161adec1152d3d028
Dani Roxberry <dani@bitoriented.com>
2019-06-10 13:24:40 -0700

    [add] skeleton for templates / forms

--- pizzashop/shop/views.py
+++ pizzashop/shop/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+
+from django.shortcuts import render, Http404, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, 'shop/home.html', {
+    })
