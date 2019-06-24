3669d0d096302f328cdf55ce0a3f9c1d94108273
Nathan Pillai <natepill@gmail.com>
2019-06-24 15:00:59 -0700

    [added] refs for heroku

--- pizzashop/pizzashop/settings.py
+++ pizzashop/pizzashop/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -139,3 +140,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ]
 }
+
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
